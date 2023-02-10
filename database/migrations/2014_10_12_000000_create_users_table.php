686a1bae4feed86f3085c66f38dbb295bd59829f
Diovi Robinet Morales <diovi@dspot.com.pl>
2023-02-10 18:46:28 +0300

    [FIX] Modifying migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name');
+            $table->integer('age');
+            $table->enum('gender', ['Female', 'Male', 'Intersex']);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
